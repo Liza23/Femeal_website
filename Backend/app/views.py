6b1d29d98bf759e3baa69511d4c12cbc8aee608d
Liza23 <lizadahiya01@gmail.com>
2021-01-22 17:14:32 +0530

    backend-added

---
+++ Backend/app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import RegUser
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def register(request):
+    print(request.GET)
+    print("HAHA")
+    if request.POST:
+        name = request.POST['name']
+        phone_number = request.POST['phone_number']
+        email = request.POST['email']
+        # del request.POST
+        x = RegUser.objects.create(name=name, phone_number = phone_number, email_address = email)
+        x.save()
+        print("done registering")
+        # request.GET =
+
+    return render(request, 'app/index.html')
