a87d9a528aa24002a31b60dc821f8cb658f8d2e8
Rockingrajat <jrajatain12@gmail.com>
2021-01-26 14:39:46 +0530

    delete outer index.html, add femeal to .gitignore

--- Backend/settings.py
+++ Backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '=ib15*5c5in&aafgr%wpbqwx)4k13zx&q_@=evu59f8t8s-&-$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['femeal.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','femeal.herokuapp.com']
 
 
 # Application definition
